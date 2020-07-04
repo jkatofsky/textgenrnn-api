170d0e16f90c8b431e9b070dc439f2fcb75c1f5a
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2020-07-04 13:42:59 -0400

    no clue how I'm going to implement the gpt code

--- gpt.py
+++ gpt.py
@@ -1,17 +1,5 @@
 # TODO: implement
 
 
-def _get_clean_sess(model_name):
-    pass
-
-
-def _finetune():
-    pass
-
-
-def _generate():
-    pass
-
-
-def finetine_and_generate(training_strings, prompt, length):
+def finetune_and_generate(training_strings, prompt, length):
     pass
