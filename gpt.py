b1865b7216357c64cc30bf775daec206dda50c90
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2020-07-04 16:21:08 -0400

    everything may be terrible, but the API returns something!

--- gpt.py
+++ gpt.py
@@ -1,5 +1,39 @@
-# TODO: implement
+import gpt_2_simple as gpt2
+import os
+import gc
+import tensorflow as tf
+import random
+
+session = gpt2.start_tf_sess(threads=1)
+gpt2.load_gpt2(session)
 
 
-def finetune_and_generate(training_strings, prompt, length):
-    pass
+# TODO: eventually need to use /tmp for any files generated below
+# TODO: play with settings for finetune and generate to speed it up
+# TODO: auto clean up files in the checkpoint folder
+# TODO: investigate output; finetune doesn't have much effect
+
+def finetune_and_generate(training_strings, prompt, num_words):
+
+    while not sum([len(string.split()) for string in training_strings]) > 1023:
+        training_strings.append(random.choice(training_strings))
+
+    tf.reset_default_graph()
+    global session
+    session = gpt2.start_tf_sess(threads=1)
+
+    with open("tmp/training.txt", "w") as training_fp:
+        for training_string in training_strings:
+            training_fp.write(training_string + '\n')
+
+    gpt2.finetune(session, "tmp/training.txt", steps=1)
+
+    text = gpt2.generate(session,
+                         prefix=prompt,
+                         length=num_words if num_words else 100,
+                         return_as_list=True)[0]
+
+    os.remove("tmp/training.txt")
+    gc.collect()
+
+    return text
--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from sanic import Sanic
 from sanic.response import json
 
-from gpt import finetune_and_generate
+import gpt
+
 
 app = Sanic(__name__)
 
 # TODO: error handling
+# TODO: accept more settings for training from the client? (i.e. temperature)
+# TODO: after this works, use /tmp and tokens/sessions to create TWO routes:
+#   - /finetune: make finetuned model, on given message set, w/ short lifespan
+#   - /generate: generate from model, if it exists and client has permission
 
 
-@app.route("/generate")
+@app.route("/generate", methods=['POST'])
 async def generate(request):
 
     data = request.json
     training_strings = data.get('training_strings')
     prompt = data.get('prompt', None)
-    length = data.get('length', None)
+    num_words = data.get('num_words', None)
 
-    output = finetune_and_generate(training_strings, prompt, length)
+    output = gpt.finetune_and_generate(training_strings, prompt, num_words)
 
-    return json({'output': output}, status=200)
+    return json({'output': output},
+                status=200,
+                headers={'Access-Control-Allow-Origin': '*'})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
