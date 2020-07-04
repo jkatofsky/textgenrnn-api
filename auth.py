7f2f115f0aa0317d8392bf6fac7bfcba54fe41e6
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2020-07-04 19:17:07 -0400

    let's not get ahead of ourselves with this token stuff

--- auth.py
+++ auth.py
@@ -1,18 +1,18 @@
-def create_token_and_model_id():
+def create_model_id():
     # TODO: implement
-    # create user token and model id
-    # associate the two
-    return None, None
+    # create unique model ID
+    return ''
 
 
-def set_expiration_time(client_token):
+def set_expiration_time(model_id):
     # TODO: implement
     # push the expiration time up a certain amount
-    # on time limit reached, destroy token and model associated w/ it
+    # on time limit reached, destroy ID and model associated w/ it
     pass
 
 
-def authenticate_token(client_token):
+def authenticate_token(model_id):
     # TODO: implement
-    # check if it's still around. if yes, get the model ID
-    return None, None
+    # check if it's still around. if yes, get the model ID.
+    # if no, second arg = false
+    return '', False
--- main.py
+++ main.py
@@ -5,23 +5,28 @@ import auth
 
 app = Sanic(__name__)
 
+ALLOW_ACCESS_HEADER = {'Access-Control-Allow-Origin': '*'}
 
 # TODO: error handling
 # TODO: accept more settings from the client? (i.e. temperature)
+# TODO: only allow one IP address to:
+#       - /finetune N models
+#       - /generate N times per minute
+# TODO: change routes to be more CRUD-y? create, re-train, delete?
 
 
-@app.route("/token-expired", methods=['POST'])
-async def token_expired(request):
+@app.route("/modelIdExpired", methods=['POST'])
+async def model_id_expired(request):
 
     data = request.json
 
-    client_token = data.get('token', None)
+    model_id = data.get('model_id', None)
 
-    _, is_expired = auth.authenticate_token(client_token)
+    _, is_expired = auth.authenticate_id(model_id)
 
     return json({'is_expired': is_expired},
                 status=200,
-                headers={'Access-Control-Allow-Origin': '*'})
+                headers=ALLOW_ACCESS_HEADER)
 
 
 @app.route("/finetune", methods=['POST'])
@@ -30,34 +35,34 @@ async def finetune(request):
     data = request.json
     training_strings = data.get('training_strings')
 
-    client_token, model_id = auth.create_token_and_model_id()
+    model_id = auth.create_model_id()
 
     gpt.finetune(model_id, training_strings)
 
-    auth.set_expiration_time(client_token)
+    auth.set_expiration_time(model_id)
 
-    return json({'token': client_token},
+    return json({'model_id': model_id},
                 status=200,
-                headers={'Access-Control-Allow-Origin': '*'})
+                headers=ALLOW_ACCESS_HEADER)
 
 
 @app.route("/generate", methods=['POST'])
 async def generate(request):
 
     data = request.json
-    client_token = data.get('token', None)
+    model_id = data.get('model_id', None)
     prompt = data.get('prompt', None)
     num_words = data.get('num_words', None)
 
-    model_id, expired = auth.authenticate_token(client_token)
+    model_id, expired = auth.authenticate_id(model_id)
 
     output = gpt.generate(model_id, prompt, num_words)
 
-    auth.set_expiration_time(client_token)
+    auth.set_expiration_time(model_id)
 
     return json({'output': output},
                 status=200,
-                headers={'Access-Control-Allow-Origin': '*'})
+                headers=ALLOW_ACCESS_HEADER)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
