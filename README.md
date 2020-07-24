b4d38a72e8b9cec2bc8168464be503c67cbea0bf
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2020-07-23 21:08:47 -0400

    flask rewrite

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-A lightweight Python API, designed to run on Google Cloud, which allows clients to train RNNs on arbitirary strings and then generate output. Uses the phenomenal [textgenrnn](https://github.com/minimaxir/textgenrnn) module for text generation and [Sanic](https://github.com/huge-success/sanic) as a web framework. `textgenrnn-api` will not output anything worthy of a NLP paper, but it's still pretty fun.
+A lightweight Python API, designed to run on Google Cloud, which allows clients to train RNNs on arbitirary strings and then generate output. Uses the phenomenal [textgenrnn](https://github.com/minimaxir/textgenrnn) module for text generation and [Flask](https://flask.palletsprojects.com/en/1.1.x/) as a web framework. `textgenrnn-api` will not output anything worthy of a NLP paper, but it's still pretty fun.
 
 ## Routes
 
@@ -41,16 +41,15 @@ A lightweight Python API, designed to run on Google Cloud, which allows clients 
 5. [Create a Google Cloud Storage Bucket](https://cloud.google.com/storage/docs/creating-buckets#storage-create-bucket-console) in your project to store the models and set the `MODEL_BUCKET_NAME` variable appropriately in `settings.py`. You can optionally set a lifespan for the models using a [delete  lifycycle rule](https://cloud.google.com/storage/docs/lifecycle?_ga=2.24563129.-2066692002.1593836412#delete).
 6. [Download a service account credentials JSON](https://cloud.google.com/storage/docs/reference/libraries#setting_up_authentication) for your project with permissions for the model bucket and set the `CREDENTIALS_JSON_PATH` variable appropriately in `settings.py`.
 
-7. To test the server locally, use the following command.
+7. To test the server locally (with convenient hot reload), use the following command.
 
    ```bash
-   sanic main.app --host=0.0.0.0 --port=8000 --debug --workers=#your number of CPU cores
+   python3 -m flask run --reload
    ```
 
-8. Assuming you have the [gcloud SDK](https://cloud.google.com/sdk/install) installed, you can deploy this repo right to App Engine:
+8. Assuming you have the [gcloud SDK](https://cloud.google.com/sdk/install) installed and have set the desired project as your current one, you can deploy this repo right to App Engine.
 
    ```bash
-   gcloud config set #your project name
    gcloud app deploy
    ```
 
--- app.py
+++ app.py
@@ -2,30 +2,28 @@ from utils import valid_training_strings, valid_model_id, valid_options
 import model_manager
 import textgen
 
-from sanic import Sanic
-from sanic.response import json
-from sanic_cors import CORS
+from flask import Flask, request
+from flask_cors import CORS
 import traceback
 
-app = Sanic(__name__)
+app = Flask(__name__)
 CORS(app)
 
-SERVER_ERROR = json({'error': 'A server error occured.'},
-                    status=500)
+SERVER_ERROR = {'error': 'A server error occured.'}, 500
 
 
 @app.route("/train", methods=['POST'])
-async def train(request):
+def train():
     try:
         data = request.json
         training_strings = data.get('training_strings', None)
         if not valid_training_strings(training_strings):
-            return json({'error': 'training_strings was not supplied properly.'}, status=400)
+            return {'error': 'training_strings was not supplied properly.'}, 400
 
         model = textgen.train(training_strings)
         model_id = model_manager.upload_model(model)
 
-        return json({'model_id': model_id}, status=200)
+        return {'model_id': model_id}, 200
 
     except Exception as e:
         traceback.print_exc()
@@ -33,20 +31,20 @@ async def train(request):
 
 
 @app.route("/generate", methods=['POST'])
-async def generate(request):
+def generate():
     try:
         data = request.json
         model_id = data.get('model_id', None)
         options = data.get('options', {})
         if not valid_model_id(model_id):
-            return json({'error': 'model_id was not supplied properly.'}, status=400)
+            return {'error': 'model_id was not supplied properly.'}, 400
         if not valid_options(options):
-            return json({'error': 'options was not supplied properly.'}, status=400)
+            return {'error': 'options was not supplied properly.'}, 400
 
         model = model_manager.download_model(model_id)
 
         if not model:
-            return json({'error': 'Model corresponding with model_id does not exist.'}, status=401)
+            return {'error': 'Model corresponding with model_id does not exist.'}, 401
 
         prompt = options.get('prompt', None)
         max_length = options.get('max_length', 300)
@@ -54,7 +52,7 @@ async def generate(request):
 
         output = textgen.generate(model, prompt, max_length, temperature)
 
-        return json({'output': output}, status=200)
+        return {'output': output}, 200
 
     except Exception as e:
         traceback.print_exc()
